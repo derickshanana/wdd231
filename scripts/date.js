63be610a0446fcff3d3292a02f26db2fca39357c
Derick Shanana <derickshanana50@gmail.com>
2025-05-14 19:23:43 +0200

    new

---
+++ scripts/date.js
@@ -0,0 +1,8 @@
+// Set current year
+const yearSpan = document.getElementById("currentyear");
+const currentYear = new Date().getFullYear();
+yearSpan.textContent = currentYear;
+
+// Set last modified date
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
